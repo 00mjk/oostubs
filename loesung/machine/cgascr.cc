ba6789003ebc254da1b5b61ce0a609ad90c218a8
Benjamin Kramer <benjamin.kramer@udo.edu>
2013-10-23 17:45:31 +0200

    Implement CGA_Screen.

--- loesung/machine/cgascr.cc
+++ loesung/machine/cgascr.cc
@@ -14,4 +14,79 @@
 
 #include "machine/cgascr.h"
 
-/* Hier muesst ihr selbst Code vervollstaendigen */ 
+static char *const CGA_START = (char *)0xb8000;
+
+void CGA_Screen::show(int x, int y, char c, unsigned char attrib) {
+  char *pos = CGA_START + 2 * (x + y * 80);
+
+  pos[0] = c;
+  pos[1] = attrib;
+}
+
+void CGA_Screen::setpos(int x, int y) {
+  IO_Port index(0x3d4);
+  IO_Port data(0x3d5);
+
+  unsigned short pos = x + (y * 80);
+  // Cursor, higher 8 bits.
+  index.outb(15);
+  data.outb((pos >> 8) & 0xff);
+
+  // Cursor, lower 8 bits.
+  index.outb(14);
+  data.outb(pos & 0xff);
+}
+
+void CGA_Screen::getpos(int &x, int &y) {
+  IO_Port index(0x3d4);
+  IO_Port data(0x3d5);
+
+  unsigned short pos = 0;
+  // Cursor, higher 8 bits.
+  index.outb(15);
+  pos |= (unsigned short)data.inb() << 8;
+
+  // Cursor, lower 8 bits.
+  index.outb(14);
+  pos |= data.inb();
+
+  x = pos % 80;
+  y = pos / 80;
+}
+
+void CGA_Screen::print(const char *text, int length, unsigned char attrib) {
+  int x, y;
+  getpos(x, y);
+
+  for (int i = 0; i != length; ++i) {
+    // '\n' behandeln.
+    if (text[i] == '\n') {
+      ++y;
+      x = 0;
+    } else {
+      // 1 Zeichen ausgeben.
+      show(x, y, text[i], attrib);
+      ++x;
+    }
+
+    // Wenn das Ende der Zeile erreicht ist umbrechen.
+    if (x >= 80) {
+      ++y;
+      x = 0;
+    }
+
+    // Wenn das Ende des Bildschirms erreicht ist nach oben scrollen.
+    if (y >= 25) {
+      // Gesamten CGA-Speicher eine Zeile nach oben kopieren.
+      for (int i = 0; i != 2 * 80 * 24; ++i)
+        CGA_START[i] = CGA_START[i + (2 * 80)];
+      // Letzte Zeile ausnullen.
+      for (int i = 2 * 80 * 24; i != 2 * 80 * 25; ++i)
+        CGA_START[i] = 0;
+      y = 24;
+    }
+  }
+
+  setpos(x, y);
+}
+
--- loesung/machine/cgascr.h
+++ loesung/machine/cgascr.h
@@ -26,7 +26,37 @@ public:
 /* Hier muesst ihr selbst Code vervollstaendigen */ 
  {}
 
-/* Hier muesst ihr selbst Code vervollstaendigen */ 
+   // Diese Methode gibt das Zeichen c an der Position (x,y) mit dem angegebenen
+   // Farbattribut attrib aus. Dabei gibt x die Spalte und y die Zeile der
+   // gewünschten Position an, wobei gilt: 0<=x<=79 und 0<=y<=24. Die Position
+   // (0,0) bezeichnet die linke obere Ecke des Bildschirms. Mit attrib lassen
+   // sich Merkmale wie Hintergrundfarbe, Vordergrundfarbe und Blinken
+   // festlegen.
+   void show(int x, int y, char c, unsigned char attrib);
+
+   // Diese Methode setzt den Cursor an die angegebene Position.
+   void setpos(int x, int y);
+
+   // Hiermit kann die aktuelle Position des Cursors von der Grafikkarte
+   // abgefragt werden. x und y sind Referenzen und dienen als Ausgabeparameter.
+   void getpos(int &x, int &y);
+
+   // Mit dieser Methode kann eine Zeichenkette text ausgegeben werden, wobei an
+   // der aktuellen Position des Cursors begonnen wird. Da die Zeichenkette
+   // anders als sonst bei C üblich keine Nullterminierung zu enthalten braucht,
+   // wird der Parameter length benötigt, der angeben muss, aus wievielen
+   // Zeichen text besteht. Nach Abschluss der Ausgabe soll der Cursor hinter
+   // dem zuletzt ausgegebenen Zeichen stehen. Der gesamte text soll einheitlich
+   // mit den durch attrib gewählten Farben dargestellt werden.
+   // Wenn bis zum Ende der Zeile nicht mehr genügend Platz ist, soll die
+   // Ausgabe auf der folgenden Zeile fortgesetzt werden. Sobald die letzte
+   // Bildschirmzeile gefüllt ist, soll der gesamte Bildschirmbereich um eine
+   // Zeile nach oben geschoben werden. Dadurch verschwindet die erste Zeile.
+   // Dafür kann nun die letzte Zeile erst gelöscht und dann die Ausgabe dort
+   // fortgesetzt werden.
+   // Ein Zeilenumbruch muss auch erfolgen, wann immer das Zeichen '\n' im
+   // auszugebenen Text enthalten ist.
+   void print(const char *text, int length, unsigned char attrib);
  };
 
 /* Hier muesst ihr selbst Code vervollstaendigen */ 
